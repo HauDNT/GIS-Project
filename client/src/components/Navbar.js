388b9326bff454a575fe669dd6669056654708d5
Thomas <tienhau.it@gmail.com>
2024-09-12 20:08:26 +0700

    Create: Page not found

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -42,7 +42,7 @@ function Navbar({settings = ['Nothing'], toggleSidebar}) {
                     </Typography>
                     <Tooltip title="Open settings">
                         <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                            <Avatar alt="User" src="/static/images/avatar/2.jpg" />
                         </IconButton>
                     </Tooltip>
                     <Menu
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
@@ -23,7 +22,7 @@ function Sidebar({ sidebarState, toggleSidebar }) {
                 {
                     ['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
                         <ListItem key={text} disablePadding>
-                            <ListItemButton>
+                            <ListItemButton onClick={toggleSidebar}>
                                 <ListItemIcon>
                                     {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
                                 </ListItemIcon>
@@ -32,19 +31,6 @@ function Sidebar({ sidebarState, toggleSidebar }) {
                         </ListItem>
                     ))
                 }
-            </List>
-            <Divider />
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
             </List>
         </Box>
     );
