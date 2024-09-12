a7e46d9242a0be2f27da9c2669d948b1f57131bc
Thomas <tienhau.it@gmail.com>
2024-09-12 22:36:52 +0700

    Update: Return JWT token and user data to client

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { StaffsService } from 'src/staffs/staffs.service';
 import { LoginDTO } from './dto/login.dto';
 import { SignupDTO } from './dto/signup.dto';
+import { UserDataReponse } from './dto/userDataResponse.dto';
 import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
 import { JWTPayloadType } from './jwt/jwt-payload-type';
@@ -28,12 +29,13 @@ export class AuthService {
         }
     }
 
-    async login(data: LoginDTO): Promise<{ accessToken: string }> {
+    async login(data: LoginDTO): Promise<UserDataReponse> {
         const isEmail = data.username.includes("@");
         const staff = await this.staffService.findOneByUsername(data.username, isEmail);
         const passwordMatched = await bcrypt.compare(data.password, staff.MatKhau);
 
         if (passwordMatched) {
+
             // Send JWT Token
             const payload: JWTPayloadType = {
                 userId: staff.id,
@@ -42,6 +44,10 @@ export class AuthService {
             };
 
             return {
+                userId: staff.id,
+                fullname: staff.HoTen,
+                email: staff.Email,
+                phoneNumber: staff.SoDienThoai,
                 accessToken: this.jwtService.sign(payload),
             }
         }
