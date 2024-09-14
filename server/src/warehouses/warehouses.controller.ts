7b1705befe2d32175d3c4f7fe61fb78deaf5c975
Thomas <tienhau.it@gmail.com>
2024-09-14 11:24:58 +0700

    Create: Fetch all warehouses API | Swagger APIs documents

--- server/src/warehouses/warehouses.controller.ts
+++ server/src/warehouses/warehouses.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { WarehousesService } from './warehouses.service';
+import { Warehouse } from './warehouse.entity';
+import { JWTGuard } from 'src/auth/jwt/jwt-guard';
 
 @Controller('warehouses')
 export class WarehousesController {
-  constructor(private readonly warehousesService: WarehousesService) {}
+    constructor(private readonly warehousesService: WarehousesService) { }
+
+    @Get('all')
+    // @UseGuards(JWTGuard)
+    getAll(): Promise<Warehouse[]> {
+        return this.warehousesService.getAll();
+    }
 }
