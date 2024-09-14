af4e126548c525a6608ed0d7d060156cb37eb177
Thomas <tienhau.it@gmail.com>
2024-09-14 11:00:35 +0700

    Create: Apply charset utf8mb4_unicode_ci for all tables with SetCharsetForAllTables1671234567890 migration

--- server/db/migrations/1726285688280-root-migrations.ts
+++ server/db/migrations/1726285688280-root-migrations.ts
@@ -1,18 +1,17 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class RootDatabase1725870250011 implements MigrationInterface {
-    name = 'RootDatabase1725870250011'
+export class RootMigrations1726285688280 implements MigrationInterface {
+    name = 'RootMigrations1726285688280'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`staffs\` (\`id\` int NOT NULL AUTO_INCREMENT, \`HoTen\` varchar(255) NOT NULL, \`Email\` varchar(255) NOT NULL, \`SoDienThoai\` varchar(255) NOT NULL, \`MatKhau\` varchar(255) NOT NULL, \`GioiTinh\` tinyint NOT NULL, \`DiaChi\` text NOT NULL, \`warehouseId\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`warehouses\` (\`id\` int NOT NULL AUTO_INCREMENT, \`TenKho\` varchar(255) NOT NULL, \`DiaChi\` varchar(255) NOT NULL, \`TungDo\` int NULL, \`HoanhDo\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`dispatch_rice\` (\`ID_DispatchSlip\` int NOT NULL, \`ID_RicePlant\` int NOT NULL, \`SoLuong\` int NOT NULL, \`DonGia\` int NOT NULL, \`dispatchSlipId\` int NULL, PRIMARY KEY (\`ID_DispatchSlip\`, \`ID_RicePlant\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`riceplants\` (\`id\` int NOT NULL AUTO_INCREMENT, \`LoaiLua\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`receiving_rice\` (\`ID_DispatchSlip\` int NOT NULL, \`ID_RicePlant\` int NOT NULL, \`SoLuong\` int NOT NULL, \`DonGia\` int NOT NULL, \`receiveSlipId\` int NULL, PRIMARY KEY (\`ID_DispatchSlip\`, \`ID_RicePlant\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`receiving_slips\` (\`id\` int NOT NULL AUTO_INCREMENT, \`ID_Warehouse\` int NOT NULL, \`ID_Customer\` int NOT NULL, \`ID_Staff\` int NOT NULL, \`ThoiGianLapDon\` datetime NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`customers\` (\`id\` int NOT NULL AUTO_INCREMENT, \`HoTen\` varchar(255) NOT NULL, \`Email\` varchar(255) NOT NULL, \`SoDienThoai\` int NOT NULL, \`GioiTinh\` tinyint NOT NULL, \`DiaChi\` text NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`dispatch_slips\` (\`id\` int NOT NULL AUTO_INCREMENT, \`ID_Warehouse\` int NOT NULL, \`ID_Customer\` int NOT NULL, \`ID_Staff\` int NOT NULL, \`ThoiGianLapDon\` datetime NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`CREATE TABLE \`warehouses\` (\`id\` int NOT NULL AUTO_INCREMENT, \`TenKho\` varchar(255) NOT NULL, \`DiaChi\` text NOT NULL, \`TungDo\` int NULL, \`HoanhDo\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`ALTER TABLE \`staffs\` ADD CONSTRAINT \`FK_f0fc53df806b23ee63fd926dec0\` FOREIGN KEY (\`warehouseId\`) REFERENCES \`warehouses\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`CREATE TABLE \`staffs\` (\`id\` int NOT NULL AUTO_INCREMENT, \`HoTen\` varchar(255) NOT NULL, \`Email\` varchar(255) NOT NULL, \`SoDienThoai\` varchar(255) NOT NULL, \`MatKhau\` varchar(255) NOT NULL, \`GioiTinh\` tinyint NOT NULL, \`DiaChi\` text NOT NULL, \`warehouseId\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`ALTER TABLE \`dispatch_rice\` ADD CONSTRAINT \`FK_713388e594fc92f6633b7b609b7\` FOREIGN KEY (\`dispatchSlipId\`) REFERENCES \`dispatch_slips\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`dispatch_rice\` ADD CONSTRAINT \`FK_1995f8be0a1ed77967616224655\` FOREIGN KEY (\`ID_RicePlant\`) REFERENCES \`riceplants\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`receiving_rice\` ADD CONSTRAINT \`FK_9d9ded7fefbdc6744876fa5abee\` FOREIGN KEY (\`receiveSlipId\`) REFERENCES \`receiving_slips\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
@@ -23,9 +22,11 @@ export class RootDatabase1725870250011 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`dispatch_slips\` ADD CONSTRAINT \`FK_3b3fbd68b64a5b2c11790084920\` FOREIGN KEY (\`ID_Customer\`) REFERENCES \`customers\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`dispatch_slips\` ADD CONSTRAINT \`FK_d308786eaf60e963d0b92899322\` FOREIGN KEY (\`ID_Staff\`) REFERENCES \`staffs\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`dispatch_slips\` ADD CONSTRAINT \`FK_70a799c08d9634440590ff3d12f\` FOREIGN KEY (\`ID_Warehouse\`) REFERENCES \`warehouses\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE \`staffs\` ADD CONSTRAINT \`FK_f0fc53df806b23ee63fd926dec0\` FOREIGN KEY (\`warehouseId\`) REFERENCES \`warehouses\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`staffs\` DROP FOREIGN KEY \`FK_f0fc53df806b23ee63fd926dec0\``);
         await queryRunner.query(`ALTER TABLE \`dispatch_slips\` DROP FOREIGN KEY \`FK_70a799c08d9634440590ff3d12f\``);
         await queryRunner.query(`ALTER TABLE \`dispatch_slips\` DROP FOREIGN KEY \`FK_d308786eaf60e963d0b92899322\``);
         await queryRunner.query(`ALTER TABLE \`dispatch_slips\` DROP FOREIGN KEY \`FK_3b3fbd68b64a5b2c11790084920\``);
@@ -36,15 +37,14 @@ export class RootDatabase1725870250011 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`receiving_rice\` DROP FOREIGN KEY \`FK_9d9ded7fefbdc6744876fa5abee\``);
         await queryRunner.query(`ALTER TABLE \`dispatch_rice\` DROP FOREIGN KEY \`FK_1995f8be0a1ed77967616224655\``);
         await queryRunner.query(`ALTER TABLE \`dispatch_rice\` DROP FOREIGN KEY \`FK_713388e594fc92f6633b7b609b7\``);
-        await queryRunner.query(`ALTER TABLE \`staffs\` DROP FOREIGN KEY \`FK_f0fc53df806b23ee63fd926dec0\``);
-        await queryRunner.query(`DROP TABLE \`warehouses\``);
+        await queryRunner.query(`DROP TABLE \`staffs\``);
         await queryRunner.query(`DROP TABLE \`dispatch_slips\``);
         await queryRunner.query(`DROP TABLE \`customers\``);
         await queryRunner.query(`DROP TABLE \`receiving_slips\``);
         await queryRunner.query(`DROP TABLE \`receiving_rice\``);
         await queryRunner.query(`DROP TABLE \`riceplants\``);
         await queryRunner.query(`DROP TABLE \`dispatch_rice\``);
-        await queryRunner.query(`DROP TABLE \`staffs\``);
+        await queryRunner.query(`DROP TABLE \`warehouses\``);
     }
 
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -3,13 +3,20 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
+// declare const module: any;
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
     app.useGlobalPipes(new ValidationPipe());
 
     const configService = app.get(ConfigService);
-    
+
     await app.listen(configService.get<number>('port'));
+
+    // if (module.hot) {
+    //     module.hot.accept();
+    //     module.hot.dispose(() => app.close());
+    // }
 }
 bootstrap();
