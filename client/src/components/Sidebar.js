bc0797ff4b96cb04904ccf8cdd1c8419b68fbeda
Thomas <tienhau.it@gmail.com>
2024-09-26 13:49:11 +0700

    Update: Redirect sidebar button

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
 import {
     Box,
     Drawer,
@@ -8,7 +9,6 @@ import {
     ListItemText,
     ListItemButton,
 } from "@mui/material";
-
 
 import {
     DashboardOutlined,
@@ -23,26 +23,30 @@ const menuItems = [
     {
         name: 'Trang chủ',
         icon: <DashboardOutlined />,
+        routeName: 'dashboard',
     },
     {
         name: 'Kho',
         icon: <MapsHomeWorkOutlined />,
+        routeName: 'warehouse',
     },
     {
         name: 'Nhân sự',
         icon: <AccountCircleOutlined />,
         submenu: [
-            { name: 'Danh sách nhân sự', icon: <MapsHomeWorkOutlined /> },
-            { name: 'Trực kho', icon: <MapsHomeWorkOutlined /> },
+            { name: 'Danh sách nhân sự', icon: <MapsHomeWorkOutlined />, routeName: 'staffs', },
+            { name: 'Trực kho', icon: <MapsHomeWorkOutlined />, routeName: 'staffs', },
         ]
     },
     {
         name: 'Khách hàng',
         icon: <PeopleAltOutlined />,
+        routeName: 'customers',
     },
 ];
 
 function Sidebar({ sidebarState, toggleSidebar }) {
+    const navigate = useNavigate();
     const [isOpen, setOpen] = useState(sidebarState);
     const [openSubmenuIndex, setOpenSubmenuIndex] = useState(null);
 
@@ -54,12 +58,22 @@ function Sidebar({ sidebarState, toggleSidebar }) {
         setOpenSubmenuIndex(openSubmenuIndex === index ? null : index);
     };
 
+    const handleRedirect = (item, routeName) => {
+        if (!item.submenu) {
+            navigate(routeName);
+        }
+        else return;
+    };
+
     const SidebarComponent = (
         <Box sx={{ width: 300 }} role="presentation">
             <List>
                 {
                     menuItems.map((item, index) => (
-                        <div key={index}>
+                        <div 
+                            key={index} 
+                            onClick={() => handleRedirect(item, item.routeName)}
+                        >
                             <ListItem disablePadding>
                                 <ListItemButton onClick={item.submenu ? (() => handleSubmenuToggle(index)) : toggleSidebar}>
                                     <ListItemIcon>
@@ -72,13 +86,13 @@ function Sidebar({ sidebarState, toggleSidebar }) {
                             {
                                 item.submenu && openSubmenuIndex === index && (
                                     <List component="div" style={{padding: 0, paddingLeft: 15}}>
-                                        {item.submenu.map((subitem, subIndex) => (
-                                            <ListItem key={subIndex} disablePadding>
-                                                <ListItemButton onClick={toggleSidebar}>
+                                        {item.submenu.map((subItem, subIndex) => (
+                                            <ListItem key={subIndex} disablePadding onClick={() => handleRedirect(subItem, subItem.routeName)}>
+                                                <ListItemButton>
                                                     <ListItemIcon>
-                                                        {subitem.icon}
+                                                        {subItem.icon}
                                                     </ListItemIcon>
-                                                    <ListItemText primary={subitem.name} />
+                                                    <ListItemText primary={subItem.name} />
                                                 </ListItemButton>
                                             </ListItem>
                                         ))}
