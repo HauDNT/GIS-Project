f1ef63a830179ac3200484d2ff38c7da514f50de
Thomas <tienhau.it@gmail.com>
2024-09-26 13:49:11 +0700

    Update: Redirect sidebar button

--- client/src/common/AxiosInstance.js
+++ client/src/common/AxiosInstance.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { getFromLocalStorage } from "../utils/LocalStorage";
 import { ServerAddress } from "./UrlServerAddress";
-import { toast } from "react-toastify";
 
 // Instance axios với cấu hình mặc định
 const axiosInstance = axios.create({
