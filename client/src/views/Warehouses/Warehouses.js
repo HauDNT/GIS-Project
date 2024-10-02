c36010f9d0a9bcb847689a1889105224d1720c92
Thomas <tienhau.it@gmail.com>
2024-10-02 15:27:56 +0700

    Create: Show warehouses were deleted and restore

--- client/src/views/Warehouses/Warehouses.js
+++ client/src/views/Warehouses/Warehouses.js
@@ -1,36 +1,41 @@
 import { useState, useEffect, useContext } from 'react';
+import { toast } from 'react-toastify';
+import { useNavigate, useHistory } from "react-router-dom";
+import {
+    Container,
+    Row,
+    Col,
+} from 'react-bootstrap';
 import axiosInstance from '../../common/AxiosInstance';
 import { WarehousesContext } from '../../context/WarehousesContext.js';
 import DataTable from "../../components/DataTable.js";
-import { toast } from 'react-toastify';
 
 function Warehouses() {
+    const navigate = useNavigate();
     const { listWarehouses, deleteWarehouse } = useContext(WarehousesContext);
     const headerNames = ['Mã kho', 'Tên kho', 'Địa chỉ', 'Kinh độ', 'Vĩ độ'];
 
-    const deleteWarehouses = async (warehouseDeletedIds) => {
-        const status = true;
-
+    const softDeleteWarehouses = async (warehouseIds) => {
         try {
-            warehouseDeletedIds.map(id => {
+            warehouseIds.map(id => {
                 axiosInstance.patch(`/warehouses/soft-delete/${id}`);
                 deleteWarehouse(id);
             });
         } catch (error) {
-            status = false;
-        }
-
-        if (!status) toast.error('Đã xảy ra lỗi trong quá trình xoá kho. Vui lòng kiểm tra lại.');
+            toast.error('Đã xảy ra lỗi trong quá trình xoá kho. Vui lòng kiểm tra lại.');
+        };
     };
 
     return (
-        listWarehouses.length > 0 &&
-        <DataTable
-            data={listWarehouses}
-            columnHeadersName={headerNames}
-            pageSize={listWarehouses.length}
-            onDelete={deleteWarehouses}
-        />
+        <Container fluid>
+            <DataTable
+                data={listWarehouses}
+                columnHeadersName={headerNames}
+                pageSize={listWarehouses.length}
+                onDelete={softDeleteWarehouses}
+                onRestore={() => navigate('restore')}
+            />
+        </Container>
     )
 }
 
--- client/src/views/index.js
+++ client/src/views/index.js
@@ -1,7 +1,15 @@
 export { default as Login } from './Auth/Login';
 export { default as Home } from './Home/Home';
 export { default as Dashboard } from './Dashboard/Dashboard';
+
+// Warehouses
 export { default as Warehouses } from './Warehouses/Warehouses';
+export { default as WarehousesRestore} from './Warehouses/Restore';
+
+// Staffs
 export { default as Staffs } from './Staffs/Staffs';
+
+// Customers
 export { default as Customers } from './Customers/Customers';
+
 export { default as PageNotFound } from './PageNotFound';
