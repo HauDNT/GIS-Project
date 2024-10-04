e23f274bd952f80afac0913ab8fd73305e85157b
Thomas <tienhau.it@gmail.com>
2024-10-04 19:29:09 +0700

    Create: Get staffs were deleted API | Update: Update some APIs's responses and services

--- server/src/utils/omit_field.utils.ts
+++ server/src/utils/omit_field.utils.ts
@@ -1,9 +1,11 @@
 export function omitFields(object: any, keys: string[]) {
     const cloneObject = { ...object };
 
-    for (const key of keys) {
-        delete cloneObject[key];
-    }
+    if (keys.length > 0) {
+        for (const key of keys) {
+            delete cloneObject[key];
+        };
+    };
 
     return cloneObject;
 }
