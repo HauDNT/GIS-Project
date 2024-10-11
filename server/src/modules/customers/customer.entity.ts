152821a3076e53139014b240ec1189102ea164af
Thomas <tienhau.it@gmail.com>
2024-10-11 22:42:06 +0700

    Create: Soft delete, restore, get customers deleted API

--- server/src/modules/customers/customer.entity.ts
+++ server/src/modules/customers/customer.entity.ts
@@ -1,9 +1,8 @@
-import { DispatchSlip } from "../dispatch_slips/dispatch_slip.entity";
-import { ReceivingSlip } from "../receiving_slips/receiving_slip.entity";
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { DispatchSlip } from '../dispatch_slips/dispatch_slip.entity';
+import { ReceivingSlip } from '../receiving_slips/receiving_slip.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
-
-@Entity("customers")
+@Entity('customers')
 export class Customer {
     @PrimaryGeneratedColumn()
     id: number;
@@ -20,15 +19,21 @@ export class Customer {
     @Column()
     Gender: boolean;
 
-    @Column("text")
+    @Column('text')
     Address: string;
 
     @Column({ type: 'varchar', nullable: true })
     imageUrl: string;
 
-    @OneToMany(() => DispatchSlip, dispatchSlips => dispatchSlips.customer)
+    @Column({ default: false })
+    isDeleted: boolean;
+
+    @Column({ type: 'timestamp', nullable: true })
+    deletedAt: Date | null;
+
+    @OneToMany(() => DispatchSlip, (dispatchSlips) => dispatchSlips.customer)
     dispatchSlips: DispatchSlip[];
 
-    @OneToMany(() => ReceivingSlip, receivingSlips => receivingSlips.customer)
+    @OneToMany(() => ReceivingSlip, (receivingSlips) => receivingSlips.customer)
     receivingSlips: ReceivingSlip[];
 }
