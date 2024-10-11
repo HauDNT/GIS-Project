42eefb1f7d627fd7efacede83cfcfe5cdb7b926d
Thomas <tienhau.it@gmail.com>
2024-10-11 14:29:33 +0700

    Update: Move all modules to folder modules | Create: response dto and helper for success/error response function

---
+++ server/src/modules/riceplants/riceplant.entity.ts
@@ -0,0 +1,18 @@
+import { DispatchRice } from "../dispatch_rices/dispatch_rice.entity";
+import { ReceivingRice } from "../receiving_rices/receiving_rice.entity";
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("riceplants")
+export class RicePlant {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    Type: string;
+
+    @OneToOne(() => ReceivingRice, receiveRice => receiveRice.ricePlant)
+    receiveRice: ReceivingRice;
+
+    @OneToOne(() => DispatchRice, dispatchRice => dispatchRice.ricePlant)
+    dispatchRice: DispatchRice;
+}
